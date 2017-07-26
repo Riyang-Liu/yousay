eb6b1f34d0dd11744d3aec34e18e27c224b10c37
Riyang Liu <riyangl@student.ucsd.edu>
2017-07-26 01:33:52 -0700

    usable on phones, added 2017summer special page navbar and top pic

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var app = express();
 
+app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.get("/", function(req, res){
-    res.render("landing.ejs");
+    res.render("landing");
 });
+
+app.get("/2017SummerSpecial", function(req, res){
+	res.render("seasonalSpecial");
+})
 
 app.listen(process.env.PORT || 3000, process.env.IP, function(){
     console.log("this is the main page");
