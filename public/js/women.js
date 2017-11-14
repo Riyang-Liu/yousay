28ee6661ca15e710ee62c5113c3aca57e86f0fc6
Riyang Liu <riyangl@student.ucsd.edu>
2017-11-13 16:13:42 -0800

    monthly update

--- public/js/women.js
+++ public/js/women.js
@@ -1,25 +1,7 @@
-//Used to generate dynamic pictures of clothes
-/*
-window.onload = function(){
+//Used to filter the categories of the clothes
 
-var cards = document.getElementById('cards');
-
-Clothes.find({}, function(err, clothes){
-    if (err) return handleError(err);
-    else {
-      for (var count = 0; count < clothes.length-1; count++){
-        console.log(count);
-      }
-    }
+$("document").ready(function(){
+  $("#top").click(function(){
+    $("#rightBox").load("/women", #rightBox);
   })
-}
-*/
-/*
-<div class="card">
-  <img class="card-img-top" src="<%= clothes[0].link %>" atl="Card image cap">
-  <div class="card-block">
-    <h5 class="card-title">blue dress</h5>
-    <p class="card-text">5400</p>
-  </div>
-</div>
-*/
+})
